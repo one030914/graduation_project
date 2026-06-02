0c920d92aed8d2c45114e3f6ffb0a87b3e08f157
one030914 <one030914@gmail.com>
2026-06-03 00:43:15 +0800

    feat: make dev:docker force to use webpack

--- project/frontend/next.config.mjs
+++ project/frontend/next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@prisma/adapter-pg", "pg", "postgres-array"],
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        ...config.watchOptions,
+        aggregateTimeout: 300,
+        ignored: /node_modules/,
+        poll: 1000,
+      };
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
